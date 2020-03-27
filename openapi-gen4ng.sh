266315929090be9b47dc09f10f205146e074459b
kinoko Matumoto <superkinoko@icloud.com>
2020-03-27 10:46:38 +0900

    Create AttendanceService

--- openapi-gen4ng.sh
+++ openapi-gen4ng.sh
@@ -7,7 +7,7 @@ for file in `\find ./definitions/ -maxdepth 1 -type f`; do
     service_kebab="${filename%%-service*}"
     service_camel=`echo $service_kebab | sed -r 's/(^|-)(.)/\U\2\E/g'`
     echo $service_camel
-    java -jar openapi-generator-cli.jar generate -i ${file} -o out/angular/${service_kebab} -g typescript-angular --additional-properties="supportsES6=true,modelPropertyNaming=original,apiModulePrefix=$service_camel,fileNaming=kebab-case,npmName=@ec-apis/$service_kebab,npmRepository=http://158.201.254.20:8081/repository/npm-private/"
+    java -jar openapi-generator-cli.jar generate -i ${file} -o out/angular/${service_kebab} -g typescript-angular --skip-validate-spec --additional-properties="supportsES6=true,modelPropertyNaming=original,apiModulePrefix=$service_camel,fileNaming=kebab-case"
 done
 
 
