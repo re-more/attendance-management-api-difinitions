c1363f56016619f376c032ee1b0d6bf893d1ac0d
Shigeki Shoji <takesection@gmail.com>
2020-03-29 02:05:58 +0900

    remove openapi-generator-cli.jar

--- openapi-gen4ng.sh
+++ openapi-gen4ng.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 rm -r out/angular/*
+
+if [[ ! -e openapi-generator-cli.jar ]]; then
+    curl -o openapi-generator-cli.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.3.0/openapi-generator-cli-4.3.0.jar
+fi
 
 for file in `\find ./definitions/ -maxdepth 1 -type f`; do
     filename=`basename ${file} .yaml`
@@ -9,5 +13,3 @@ for file in `\find ./definitions/ -maxdepth 1 -type f`; do
     echo $service_camel
     java -jar openapi-generator-cli.jar generate -i ${file} -o out/angular/${service_kebab} -g typescript-angular --skip-validate-spec --additional-properties="supportsES6=true,modelPropertyNaming=original,apiModulePrefix=$service_camel,fileNaming=kebab-case"
 done
-
-
